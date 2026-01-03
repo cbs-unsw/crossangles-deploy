0f3eba52aa19d163b91c1db3d9b32daafda22e3d
macrus <arcueun@gmail.com>
2026-01-03 23:11:28 +1100

    Update scraper/scraper/unsw/TimetableScraper.ts
    Co-authored-by: David Adams <david.adams@cross-code.org>

--- scraper/scraper/unsw/TimetableScraper.ts
+++ scraper/scraper/unsw/TimetableScraper.ts
@@ -304,7 +304,7 @@ export function shouldSkipStream(data: StreamTableData) {
 }
 
 export function shouldSkipTime(time: ClassTime) {
-  if (weeksAreOutsideTerm(time.weeks) || isIntensive(time.time)) return false;
+  if (weeksAreOutsideTerm(time.weeks) || isIntensive(time.time)) return true;
 
   return false;
 }
